895cabbae3e4d96bfa7a70f2f8f1d0d0c49d8cc7
RosemaryMac <rmachanja14@gmail.com>
2022-06-02 10:23:30 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main()
 {
-fwrite("and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n");
+char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+putchar(ch);
 return (1);
 }
