a6f7951392c124089ac7bc39a4e4c163270895f6
RosemaryMac <rmachanja14@gmail.com>
2022-06-02 12:31:34 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * prints standard error
- *
- * Return: Always 1(Error)
- */
+* main -Entry point
+*
+* Return: Always 1(Error)
+*/
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDOUT_FILENO, ch, 59);
 return (1);
 }
