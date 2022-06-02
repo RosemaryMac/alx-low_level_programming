3b594d336b460f1b30f690ed93ffe2aff250f59c
RosemaryMac <rmachanja14@gmail.com>
2022-06-02 09:31:33 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n", 59);
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
