487b9b816530a5226fbffe48f6086899ace16239
RosemaryMac <rmachanja14@gmail.com>
2022-06-09 11:18:34 -0700

    fiz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,21 @@
 int main(void)
 {
 int num;
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
 for (num = 1; num <= 100; num++)
 {
-if ((num % 3) == 0 && (num % 5) == 0)
-printf("FizzBuzz");
-else if ((num % 3) == 0)
-printf("Fizz");
-else if ((num % 5) == 0)
-printf("Buzz");
-else
-printf("%d", num);
 if (num == 100)
-continue;
-printf(" ");
+printf("%s", b);
+else if ((num % 3 == 0) && (num % 5 == 0))
+printf("%s ", fb);
+else if (num % 3 == 0)
+printf("%s ", f);
+else if (num % 5 == 0)
+printf("%s ", b);
+else
+printf("%d ", num);
 }
 printf("\n");
 return (0);
