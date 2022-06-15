f80cd5287885276cb3d653b8cac79e1c86a7d112
RosemaryMac <rmachanja14@gmail.com>
2022-06-15 12:06:35 -0700

    rot13key

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,7 +8,7 @@ char *rot13(char *s)
 {
 int indx1 = 0, indx2;
 char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-        	      'G', 'H', 'I', 'J', 'K', 'L',
+		      'G', 'H', 'I', 'J', 'K', 'L',
 		      'M', 'N', 'O', 'P', 'Q', 'R',
 		      'S', 'T', 'U', 'V', 'W', 'X',
 		      'Y', 'Z', 'a', 'b', 'c', 'd',
@@ -24,7 +24,7 @@ char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		      'r', 's', 't', 'u', 'v', 'w',
 		      'x', 'y', 'z', 'a', 'b', 'c',
 		      'd', 'e', 'f', 'g', 'h', 'i',
-		      'j', 'k', 'l','m'};
+		      'j', 'k', 'l', 'm'};
 while (s[indx1])
 {
 for (indx2 = 0; indx2 < 52; indx2++)
