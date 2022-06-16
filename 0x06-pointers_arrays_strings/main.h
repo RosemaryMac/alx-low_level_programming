439900e16281ee87edaea8a9400701ca8ef93e19
RosemaryMac <rmachanja14@gmail.com>
2022-06-16 02:39:33 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 int _putchar(char c);
