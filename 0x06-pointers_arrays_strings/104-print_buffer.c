94743fe6aa37130ca49233cd19f0571bfd7dca18
RosemaryMac <rmachanja14@gmail.com>
2022-06-17 09:03:33 -0700

    4

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,7 +12,7 @@ void print_buffer(char *b, int size)
 int byte, index;
 for (byte = 0; byte < size; byte += 10)
 {
-printf("%08x:", byte);
+printf("%08x: ", byte);
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
