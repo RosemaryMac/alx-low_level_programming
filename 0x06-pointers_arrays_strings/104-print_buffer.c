c60fac58fdaee3461663ccfe7251c7acc629f255
RosemaryMac <rmachanja14@gmail.com>
2022-06-18 02:21:25 -0700

    re

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-/*
+/**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
  *               the byte position, then showing the hex content,
  *               then displaying printable characters.
@@ -9,33 +9,39 @@
  */
 void print_buffer(char *b, int size)
 {
-int byte, index;
-for (byte = 0; byte < size; byte += 10)
-{
-printf("%08x: ", byte);
-for (index = 0; index < 10; index++)
-{
-if ((index + byte) >= size)
-printf(" ");
-else
-printf("%02x", *(b + index + byte));
-if ((index % 2) != 0 && index != 0)
-printf(" ");
-}
-for (index = 0; index < 10; index++)
-{
-if ((index + byte) >= size)
-break;
-else if (*(b + index + byte) >= 31 &&
-*(b + index + byte) <= 126)
-printf("%c", *(b + index + byte));
-else
-printf(".");
-}
-if (byte >= size)
-continue;
-printf("\n");
-}
+int i, j;
+i = 0;
 if (size <= 0)
-printf("\n");
+putchar('\n');
+else
+{
+while (i < size)
+{
+printf("%08x: ", i);
+j = 0;
+while (j < 10)
+{
+if (j % 2 == 0 && j > 0)
+printf(" ");
+if (j + i > size - 1)
+printf(" ");
+else
+printf("%.2x", b[j + i]);
+j++;
+}
+putchar(' ');
+j = 0;
+while (j < 10)
+{
+if (j + i > size - 1)
+break;
+if (b[j + i] >= ' ' && b[j + i] <= '~')
+putchar(b[j + i]);
+else
+putchar('.');
+j++;
+}
+putchar('\n');
+i += 10;
+}
 }
