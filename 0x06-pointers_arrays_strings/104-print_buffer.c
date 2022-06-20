954017d4d7e01ec1ee2c6c17c0e667a508718af6
RosemaryMac <rmachanja14@gmail.com>
2022-06-20 07:32:51 -0700

    pfbt

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,55 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_buffer - prints a buffer
-* @c: buffer to print
-* @s: bytes of buffer to print
-* @l: line of buffer to print
-* Return: void
-*/
-void print_buffer(char *c, int s, int l)
-{
-int j, k;
-for (j = 0; j <= 9; j++)
-{
-if (j <= s)
-printf("%02x", c[l * 10 + j]);
-else
-printf(" ");
-if (j % 2)
-putchar(' ');
-}
-for (k = 0; k <= s; k++)
-{
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-putchar(c[l * 10 + k]);
-else
-putchar('.');
-}
-}
-/**
-* print_buffer - prints a buffer
-* @b: buffer to print
-* @size: size of buffer
+* print_buffer - Prints a buffer 10 bytes at a time,starting with
+*                the byte position, then showing the hex content,
+*                then displaying printable characters.
+* @b: buffer to be printed
+* @size: The number of bytes to be printed from buffer
 * Return: void
 */
 void print_buffer(char *b, int size)
 {
-int i;
-for (i = 0; i <= (size - 1) / 10 && size; i++)
+int byte, index;
+for (byte = 0; byte < size; byte += 10)
 {
-printf("%08x: ", i * 10);
-if (i < size / 10)
+printf("%08x: ", byte);
+for (index = 0; index < 10; index++)
 {
-print_buffer(b, 9, i);
-}
+if ((index + byte) >= size)
+printf(" ");
 else
+printf("%02x", *(b + index + byte));
+if ((index % 2) != 0 && index != 0)
+printf(" ");
+}
+for (index = 0; index < 10; index++)
 {
-print_buffer(b, size % 10 - l, i);
+if ((index + byte) >= size)
+break;
+else if (*(b + index + byte) >= 31 &&
+	  *(b + index + byte) <= 126)
+printf("%c", *(b + index + byte));
+else
+printf(".");
 }
-putchar('\n');
+if (byte >= size)
+continue;
+printf("\n");
 }
-}
-if (size == 0)
-putchar('\n');
+if (size <= 0)
+printf("\n");
 }
