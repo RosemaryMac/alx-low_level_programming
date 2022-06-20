fec4fb1422adb16c16cd5872a039f2f3cec46245
RosemaryMac <rmachanja14@gmail.com>
2022-06-20 03:12:54 -0700

    memset0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @n: The number of bytes to be filled.
  * Return: A pointer to the filled memory area @s.
  */
-char *_memset(char *s, int c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 char *start = s;
 while (n--)
