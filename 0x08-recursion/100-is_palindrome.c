a5ac810a82b121606bf28d8489fab7e02c48c4bb
RosemaryMac <rmachanja14@gmail.com>
2022-06-22 03:00:19 -0700

    100palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -38,7 +38,7 @@ int helper_palindrome(char *s, int len)
 {
 if (len <= 1)
 return (1);
-else if (*s == *(s + len -1))
+else if (*s == *(s + len - 1))
 {
 return (helper_palindrome(s + 1, len - 2));
 }
