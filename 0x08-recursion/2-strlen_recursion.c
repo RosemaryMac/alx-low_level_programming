e3ccb0bd4d984a262f55ce66b8991a9d3f86d5c3
RosemaryMac <rmachanja14@gmail.com>
2022-06-22 00:51:51 -0700

    stringlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * @s: An input string to printing
  * Return: The length of the string
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 if (*s)
 return (1 + _strlen_recursion(s + 1));
