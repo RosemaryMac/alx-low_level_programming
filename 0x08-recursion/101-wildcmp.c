86652908086a40665cf5598e4e3baf7be87f13c1
RosemaryMac <rmachanja14@gmail.com>
2022-06-22 09:50:51 -0700

    cmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,29 @@
 #include "main.h"
 /**
- * wildcmp - A function that compares two strings
+ * wildcmp - A function that compares two strings with possible wildcards
  * @s1: First input string
  * @s2: Second input string
  * Return: 1 if strings identical and 0 in otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-if (!*s1 && !*s2)
+if (*s1 == '\0' && *s2 == '\0')
 return (1);
-if (*s1 == *s2)
-return (wildcmp(s1, s2 + 1));
-if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s2 + 1, s2)))
-return (1);
-if (*s2 == '*' && *(s1 + 1) && *s2)
+else if (*s1 == '\0' && *s2 == '*')
+return (wildcmp(s1, (s2 + 1)));
+else if (*s1 == '\0' || *s2 == '\0')
 return (0);
+if (*s1 == *s2)
+return (wildcmp((s1 + 1), (s2 + 1)));
+if (*s1 == *s2)
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, (s2 + 1)));
+else if (wildcmp(s1, s2 + 1))
+return (1);
+else
+return (wildcmp(s1 + 1, s2));
+}
 return (0);
 }
