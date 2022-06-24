f60f87241a63649da94146fbcfb3bc4833b1461d
RosemaryMac <rmachanja14@gmail.com>
2022-06-24 07:13:58 -0700

    pt

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,16 +2,12 @@
 /**
  * _puts - Write a function that prints a string, followed
  * by a new line, to stdout.
- * @str: an input string
+ * @s: an input string
  * Return: Nothing
  */
-void _puts(char *str)
+void _puts(char *s)
 {
-  int i = 0;
-while (str[i] != '\0')
-  {
-    _putchar(str[i]);
-    i++;
-  }
-_putchar('\n');
+  while (*s)
+      _putchar(*s++);
+   _putchar('\n');
 }
