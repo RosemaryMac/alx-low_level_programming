06a8735951f6ef5c4fc3c76cdf160827551f31d8
RosemaryMac <rmachanja14@gmail.com>
2022-06-26 23:18:52 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,16 +10,15 @@
 char *_strdup(char *str)
 {
 char *duplicate;
-int index, len = 0;
+int i, len = 0;
 if (str == NULL)
 return (NULL);
-for (index = 0; str[index]; index++)
+while (str[len])
 len++;
 duplicate = malloc(sizeof(char) * (len + 1));
 if (duplicate == NULL)
 return (NULL);
-for (index = 0; str[index]; index++)
-duplicate[index] = str[index];
-duplicate[len] = '\0';
+while ((duplicate[i] = str[i]) != '\0')
+i++;
 return (duplicate);
 }
