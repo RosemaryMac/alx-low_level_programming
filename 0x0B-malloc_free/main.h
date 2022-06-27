4d6d726c128e2c41f636f6e33f8ef38270087024
RosemaryMac <rmachanja14@gmail.com>
2022-06-26 22:29:13 -0700

    mainh

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#define MAIN_H
+/**
+ * Desc - Header file with functions used in the 0x0B-malloc_free directory
+ */
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+
+#endif
