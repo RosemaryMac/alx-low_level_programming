5c4984423b6efba8b24e48830d4a516448c647ba
RosemaryMac <rmachanja14@gmail.com>
2022-07-06 08:19:11 -0700

    var_func.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
+/**
+ * Desc - Header file with prototype functions used in the 0x10-variadic_functions directory.
+ */
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
