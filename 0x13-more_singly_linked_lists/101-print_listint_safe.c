d7e891b9d30669f2c8f2a35db83b151fc41fbe83
RosemaryMac <rmachanja14@gmail.com>
2022-07-19 05:58:10 -0700

    listint

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -23,8 +23,6 @@ size_t print_listint_safe(const listint_t *head)
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-		if (diff == NULL)
-			exit(98);
 	}
 
 	return (num);
