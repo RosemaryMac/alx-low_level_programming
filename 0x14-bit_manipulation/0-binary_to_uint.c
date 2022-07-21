14ce4e60d8888cc27399953a8d61f1b746289439
RosemaryMac <rmachanja14@gmail.com>
2022-07-21 04:27:45 -0700

    uint-decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int decimal = 0, rem;
+	unsigned int decimal = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b != 0; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		rem = b[i] % 10;
-		b[i] /= 10;
-		decimal += rem * pow(2, b[i] - '0');
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 
 	return (decimal);
